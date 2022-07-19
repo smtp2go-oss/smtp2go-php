c46721d5d3c863de502e28f1c4a57c5cd85facd2
Kris Johansen <kris.johansen@gmail.com>
2022-07-20 10:41:02 +1200

    add tests for duplicate/unique headers

--- tests/Unit/SendSettersTest.php
+++ tests/Unit/SendSettersTest.php
@@ -12,19 +12,19 @@ use SMTP2GO\Collections\Mail\CustomHeaderCollection;
 
 /**
  * @covers \SMTP2GO\Service\Mail\Send
-* @covers \SMTP2GO\Collections\Collection::current
-* @covers \SMTP2GO\Collections\Collection::next
-* @covers \SMTP2GO\Collections\Collection::rewind
-* @covers \SMTP2GO\Collections\Collection::valid
-* @covers \SMTP2GO\Collections\Mail\AddressCollection::__construct
-* @covers \SMTP2GO\Collections\Mail\AddressCollection::add
-* @covers \SMTP2GO\Collections\Mail\AttachmentCollection::__construct
-* @covers \SMTP2GO\Collections\Mail\CustomHeaderCollection::__construct
-* @covers \SMTP2GO\Types\Mail\Address::__construct
-* @covers \SMTP2GO\Types\Mail\Address::getEmail
-* @covers \SMTP2GO\Types\Mail\Address::getName
-* @covers \SMTP2GO\Collections\Mail\CustomHeaderCollection::add
-* @covers \SMTP2GO\Types\Mail\CustomHeader::__construct
+ * @covers \SMTP2GO\Collections\Collection::current
+ * @covers \SMTP2GO\Collections\Collection::next
+ * @covers \SMTP2GO\Collections\Collection::rewind
+ * @covers \SMTP2GO\Collections\Collection::valid
+ * @covers \SMTP2GO\Collections\Mail\AddressCollection::__construct
+ * @covers \SMTP2GO\Collections\Mail\AddressCollection::add
+ * @covers \SMTP2GO\Collections\Mail\AttachmentCollection::__construct
+ * @covers \SMTP2GO\Collections\Mail\CustomHeaderCollection::__construct
+ * @covers \SMTP2GO\Types\Mail\Address::__construct
+ * @covers \SMTP2GO\Types\Mail\Address::getEmail
+ * @covers \SMTP2GO\Types\Mail\Address::getName
+ * @covers \SMTP2GO\Collections\Mail\CustomHeaderCollection::add
+ * @covers \SMTP2GO\Types\Mail\CustomHeader::__construct
  */
 class SendSettersTest extends TestCase
 {
@@ -118,6 +118,48 @@ class SendSettersTest extends TestCase
         $this->assertEquals($headers, $this->sender->getCustomHeaders());
     }
 
+    /**
+     * @return void 
+     * @throws InvalidArgumentException 
+     * @throws Exception 
+     * @throws ExpectationFailedException 
+     * @throws InvalidArgumentException 
+     */
+    public function testSettingCustomHeaderTwiceReplacesTheFirstHeaderWhenSaidHeaderisOnlyAllowedOnce()
+    {
+        $headers = new CustomHeaderCollection([new CustomHeader('Reply-To', 'someone@email.test')]);
+        $headerItems = $headers->getItems();
+        $this->assertCount(1, $headerItems);
+        $this->assertEquals('someone@email.test', $headerItems[0]->getValue());
+
+
+        $headers->add(new CustomHeader('Reply-To', 'somebodyelse@email.test'));
+        $headerItems = $headers->getItems();
+        $this->assertCount(1, $headerItems);
+        $this->assertEquals('somebodyelse@email.test', $headerItems[0]->getValue());
+    }
+
+
+    /**
+     * @return void 
+     * @throws InvalidArgumentException 
+     * @throws Exception 
+     * @throws ExpectationFailedException 
+     * @throws InvalidArgumentException 
+     */
+    public function testSettingCustomHeaderTwiceSetsTwoHeadersWhenDuplicatesAreAllowed()
+    {
+        $headers = new CustomHeaderCollection([new CustomHeader('comments', 'a comment')]);
+        $headerItems = $headers->getItems();
+        $this->assertCount(1, $headerItems);
+        $this->assertEquals('a comment', $headerItems[0]->getValue());
+
+
+        $headers->add(new CustomHeader('comments', 'another comment'));
+        $headerItems = $headers->getItems();
+        $this->assertCount(2, $headerItems);
+        $this->assertEquals('another comment', $headerItems[1]->getValue());
+    }
 
     public function testGetMethod()
     {
