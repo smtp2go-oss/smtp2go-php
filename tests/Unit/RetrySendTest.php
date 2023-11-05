2edaffc827d89876b45e8a82eecd25eebfe551b5
Kris Johansen <kris.johansen@gmail.com>
2023-11-06 11:21:33 +1300

    use mock handler for test

--- tests/Unit/RetrySendTest.php
+++ tests/Unit/RetrySendTest.php
@@ -18,11 +18,38 @@ class RetrySendTest extends TestCase
     public function test_setting_retry()
     {
         //@todo this needs to use a mock handler rather than a real api call
-        $service = new Service('stats/email_summary',[]);
-        $apiClient = new ApiClient(SMTP2GO_API_KEY);
-        $apiClient->setMaxSendAttempts(5);
+
+        $expectedResponse = '{"request_id": "65e68938-c332-11eb-8a00-f23c9216ceac", "data": {"emails": 56, "rejects": 0, "softbounces": 4, "hardbounces": 0, "bounce_percent": "7.14"}}';
+
+        $mockResponses = new MockHandler([new Response(503, [], 'Service Unavailable'), new Response(200, [], $expectedResponse)]);
+        $handler = HandlerStack::create($mockResponses);
+        $httpClient   = new Client(['handler' => $handler]);
+
+        $service = new Service('stats/email_bounces');
+        $apiClient  = new ApiClient(SMTP2GO_API_KEY);
+
+        $apiClient->setHttpClient($httpClient);
+
+        $apiClient->setRequestOptions(['verify' => false]);
+
+
+        $apiClient->setMaxSendAttempts(2);
         $apiClient->setTimeout(2);
         $apiClient->setTimeoutIncrement(1);
-        $this->assertTrue($apiClient->consume($service));
+
+        $result = $apiClient->consume($service);
+
+
+
+        $this->assertTrue($result);
+        $this->assertNotEmpty($apiClient->getApiServerIps(false));
+        $this->assertEquals(1, $apiClient->getFailedAttempts());
+
+
+
+
+
+
+        // $this->assertTrue();
     }
 }
