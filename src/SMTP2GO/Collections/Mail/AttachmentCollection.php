6eccb6a2b87ad2932f1ee03614e0225d129a3942
Kris Johansen <kris.johansen@gmail.com>
2024-08-16 14:56:25 +1200

    allow new fileAttachment type in collection

--- src/SMTP2GO/Collections/Mail/AttachmentCollection.php
+++ src/SMTP2GO/Collections/Mail/AttachmentCollection.php
@@ -4,6 +4,7 @@ namespace SMTP2GO\Collections\Mail;
 
 use SMTP2GO\Types\Mail\Attachment;
 use SMTP2GO\Collections\Collection;
+use SMTP2GO\Types\Mail\FileAttachment;
 
 class AttachmentCollection extends Collection
 {
@@ -18,7 +19,7 @@ class AttachmentCollection extends Collection
 
     public function add($attachment)
     {
-        if (is_a($attachment, Attachment::class)) {
+        if (is_a($attachment, Attachment::class) || is_a($attachment, FileAttachment::class)) {
             $this->items[] = $attachment;
         } else {
             throw new \InvalidArgumentException('This collection expects objects of type ' . Attachment::class, ' but recieved ' . get_class($attachment));
